19d3a67e1cddaeb18f3a990d6535f8fc9c6dad0b
DragonMeow69 <140520833+DragonMeow69@users.noreply.github.com>
2023-12-29 20:44:29 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
+function saveFoodContentAsImage() {
+  var recipeOutput = document.getElementById('recipeOutput');
+  var id = Date.now()
+
+  html2canvas(recipeOutput).then(function(canvas) {
+      var imgData = canvas.toDataURL('image/png');
+      var link = document.createElement('a');
+      link.href = imgData;
+      link.download = `recipe${id}.png`;
+      link.click();
+  });
+}
 
